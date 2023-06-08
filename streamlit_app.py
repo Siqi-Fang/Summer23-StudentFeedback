de121359d8f20c3ee2ce16b3d0b9e2fa6b6ce098
Siqi-Fang <sfang@hamilton.edu>
2023-06-08 17:54:20 -0400

    trytry

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ df_overall['date'] = df_overall['date'].dt.strftime('%Y-%m-%d')
 df_today = create_data(service, 1)
 
 # ==== NUMEBRS ====
-avg_score_overall = df_overall['rating'].mean()
-avg_score_today = df_today['rating'].mean()
+avg_score_overall = df_overall['rating'].mean().round(2)
+avg_score_today = df_today['rating'].mean().round(2)
 num_resp_today = df_today['rating'].count()
 
 # ==== FIGS ====
@@ -34,13 +34,21 @@ fig_rating_by_class_by_date = rating_by_date_class(df_overall)
 col1, col2, col3 = st.columns(3)
 col1.metric("Overall Rating", avg_score_overall)
 col2.metric("Todays Rating", avg_score_today)
-col3.metric("# Response Today", num_resp_today)
+col3.metric("Feedback Received Today", num_resp_today)
 
 st.plotly_chart(fig_avg_rating_overall, theme="streamlit",
                 use_container_width=True)
-st.plotly_chart(fig_rating_by_class_overall, theme="streamlit",
-                use_container_width=True)
 st.plotly_chart(fig_rating_overtime, theme="streamlit",
                 use_container_width=True)
-st.plotly_chart(fig_rating_by_class_by_date, theme="streamlit",
+
+numbers, text = st.tabs(["📈 Ratings", "🗃 Qualitative Feedback"])
+
+numbers.subheader("Ratings")
+numbers.plotly_chart(fig_rating_by_class_overall, theme="streamlit",
                 use_container_width=True)
+numbers.plotly_chart(fig_rating_by_class_by_date, theme="streamlit",
+                use_container_width=True)
+
+text.subheader("Text feedback")
+text.write("TBD")
+
