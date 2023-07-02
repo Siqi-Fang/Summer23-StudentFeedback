1b51dab5831b283dfa3e795813c6307dc16aece6
Siqi-Fang <sfang@hamilton.edu>
2023-07-02 14:47:18 -0400

    update layout

--- utils.py
+++ utils.py
@@ -251,6 +251,7 @@ def bar_rating_by_class(df, overall):
         rating_by_class, x='name', y='rating', title='2023 Summer Camp Overall Rating by Class')
 
     fig_rating_by_class.update_layout(
+        height=500,
         xaxis={'categoryorder': 'total ascending'})
     # Display the chart
     return fig_rating_by_class
